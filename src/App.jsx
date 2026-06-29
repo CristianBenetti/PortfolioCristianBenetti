de41538bcfa9355ca829c4db183592a874e682fb
CristianBenetti <cristianjavierbenetti@gmail.com>
2026-06-29 09:25:08 -0300

    Primer commit: entrega del portfolio

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-100">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/proyectos" element={<Projects />} />
+          <Route path="/contacto" element={<Contact />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
